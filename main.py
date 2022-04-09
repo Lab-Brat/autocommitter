ee1a4ba12c3319e862ccafaf9139f68c393ed9ba
Lab-Brat <xoovoox@protonmail.com>
2022-04-09 22:41:26 +0300

    uncomment lines that start the committer service

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from committer import *
 # add systemd service and timer
 cmt = Committer()
 cmt.add_timer()
-# os.system(f'systemctl --user daemon-reload')
-# os.system(f'systemctl --user start committer.service')
+os.system(f'systemctl --user daemon-reload')
+os.system(f'systemctl --user start committer.service')
 
